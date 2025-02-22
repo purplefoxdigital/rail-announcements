8d5a4a896dd5c0a4078cfb8d7c6afa6921e9ae2b
Jack Battersby <56895948+purplefoxdigital@users.noreply.github.com>
2025-02-22 00:06:25 +0000

    feat: add all supertram stations and route selection

--- src/announcement-data/systems/rolling-stock/StadlerSupertram.tsx
+++ src/announcement-data/systems/rolling-stock/StadlerSupertram.tsx
@@ -4,6 +4,7 @@ import AnnouncementSystem, { AudioItem, CustomAnnouncementTab } from '../../Anno
 import { AllStationsTitleValueMap } from '@data/StationManipulators'
 
 interface IDepartingStopAnnouncementOptions {
+  routeName: string
   terminatesAtCode: string
   nextStationCode: string
 }
@@ -20,7 +21,24 @@ export default class StadlerSupertram extends AnnouncementSystem {
     files.push(
       'hold tight please',
       'bingbong',
-      'this is the tram train service to',
+    )
+
+    switch (options.routeName) {
+      case 'Blue':
+        files.push('this is the blue route service to')
+        break
+      case 'Purple':
+        files.push('this is the purple route service to')
+        break
+      case 'Yellow':
+        files.push('this is the yellow route service to')
+        break
+      case 'Tram Train':
+        files.push('this is the tram train service to')
+        break
+    }
+
+    files.push(
       `stations.${options.terminatesAtCode}`,
       'the next stop by request will be',
       `stations.${options.nextStationCode}`,
@@ -33,16 +51,65 @@ export default class StadlerSupertram extends AnnouncementSystem {
 
     await this.playAudioFiles(files, download)
   }
-  private AllStationFiles = [
-    'ATE',
-    'CAE',
-    'CAX',
-    'CIN',
-    'FIZ',
-    'HYP',
-    'SAI',
-    'SHQ',
-    'VAE'
+
+  private Routes = [
+    'Blue',
+    'Purple',
+    'Yellow',
+    'Tram Train',
+  ]
+
+  private AllStations = [
+    { code: 'ARR', name: 'Arborthorne Road' },
+    { code: 'ARS', name: 'Arena / Olympic Legacy Park' },
+    { code: 'ATT', name: 'Attercliffe' },
+    { code: 'BAS', name: 'Bamforth Street' },
+    { code: 'BDH', name: 'Beighton / Drake House Lane' },
+    { code: 'BIL', name: 'Birley Lane' },
+    { code: 'BMR', name: 'Birley Moor Road' },
+    { code: 'CAR', name: 'Carbrook (IKEA)' },
+    { code: 'CAS', name: 'Castle Square' },
+    { code: 'CAT', name: 'Cathedral' },
+    { code: 'CIH', name: 'City Hall' },
+    { code: 'CIR', name: 'Cricket Inn Road' },
+    { code: 'CRP', name: 'Crystal Peaks' },
+    { code: 'DOW', name: 'Donetsk Way' },
+    { code: 'FIS', name: 'Fitzalan Square' },
+    { code: 'GLT', name: 'Gleadless Townend' },
+    { code: 'GRR', name: 'Granville Road / The Sheffield College' },
+    { code: 'HAC', name: 'Hackenthorpe' },
+    { code: 'HAL', name: 'Halfway' },
+    { code: 'HEP', name: 'Herdings Park' },
+    { code: 'HIL', name: 'Hillsborough' },
+    { code: 'HIP', name: 'Hillsborough Park' },
+    { code: 'HLR', name: 'Herdings / Leighton Road' },
+    { code: 'HOL', name: 'Hollinsend' },
+    { code: 'HYP', name: 'Hyde Park' },
+    { code: 'INR', name: 'Infirmary Road' },
+    { code: 'LEL', name: 'Leppings Lane' },
+    { code: 'LPV', name: 'Langsett / Primrose View' },
+    { code: 'MAB', name: 'Malin Bridge' },
+    { code: 'MEI', name: 'Meadowhall Interchange' },
+    { code: 'MID', name: 'Middlewood' },
+    { code: 'MOW', name: 'Moss Way' },
+    { code: 'MTE', name: 'Manor Top' },
+    { code: 'NER', name: 'Netherthorpe Road' },
+    { code: 'NUS', name: 'Nunnery Square' },
+    { code: 'PAG', name: 'Park Grange' },
+    { code: 'PGC', name: 'Park Grange Croft' },
+    { code: 'PGR', name: 'Rotherham Parkgate' },
+    { code: 'ROT', name: 'Rotherham Central' },
+    { code: 'SHA', name: 'Shalesmoor' },
+    { code: 'SHS', name: 'Sheffield Station / Sheffield Hallam University' },
+    { code: 'SPL', name: 'Spring Lane' },
+    { code: 'TIN', name: 'Tinsley / Meadowhall South' },
+    { code: 'UOS', name: 'University of Sheffield' },
+    { code: 'VAC', name: 'Valley Centertainment' },
+    { code: 'WAT', name: 'Waterthorpe' },
+    { code: 'WED', name: 'Westfield' },
+    { code: 'WES', name: 'West Street' },
+    { code: 'WHL', name: 'White Lane' },
+    { code: 'WOR', name: 'Woodbourne Road' },
   ]
 
   readonly customAnnouncementTabs: Record<string, CustomAnnouncementTab<string>> = {
@@ -50,22 +117,29 @@ export default class StadlerSupertram extends AnnouncementSystem {
       name: 'Departing station',
       component: CustomAnnouncementPane,
       defaultState: {
-        terminatesAtCode: this.AllStationFiles[0],
-        nextStationCode: this.AllStationFiles[0],
+        routeName: this.Routes[0],
+        terminatesAtCode: this.AllStations[0].code,
+        nextStationCode: this.AllStations[0].code,
       },
       props: {
         playHandler: this.playDepartingStopAnnouncement.bind(this),
         options: {
+          routeName: {
+            name: 'Route name',
+            default: this.Routes[0],
+            options: this.Routes.map(route => ({ title: route, value: route })),
+            type: 'select',
+          },
           terminatesAtCode: {
             name: 'Terminates at',
-            default: this.AllStationFiles[0],
-            options: AllStationsTitleValueMap.filter(s => this.AllStationFiles.includes(s.value)),
+            default: this.AllStations[0].code,
+            options: this.AllStations.map(s => ({ title: s.name, value: s.code })),
             type: 'select',
           },
           nextStationCode: {
             name: 'Next station',
-            default: this.AllStationFiles[0],
-            options: AllStationsTitleValueMap.filter(s => this.AllStationFiles.includes(s.value)),
+            default: this.AllStations[0].code,
+            options: this.AllStations.map(s => ({ title: s.name, value: s.code })),
             type: 'select',
           },
         },
